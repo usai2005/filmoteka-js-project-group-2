cc335cd0598039b9e037f1d11da636c4465c0214
Victoria Fedkova <63882255+Victoria-Fedkova@users.noreply.github.com>
2023-04-13 10:27:34 +0300

    Create api client
    Api client для надсилання та обробки запитів до серверу

---
+++ src/js/api-client.js
@@ -0,0 +1,207 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+//зображення що завантажиться при відсутності постеру
+const DEFAULT_IMG =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png';
+
+class ApiClient {
+  constructor() {
+    this.query = '';
+    this.curentPage = 1;
+    this.totalPages = 0;
+    this.totalMovies = 0;
+    this.genres = [];
+  }
+
+  //додаткова функція що при первинному запиті та отриманні популярних фільмів робить запит на сервер і отримує масив всіх жанрів та їх id
+  async getGenres() {
+    const response = await axios.get(
+      `${BASE_URL}/genre/movie/list?api_key=ae38d5c8baf36c9c4ca14e9456f3c0fd&language=en-US`
+    );
+
+    this.genres = response.data.genres;
+  }
+
+  // підставляє до знайдених id жанрів його назву, повертає масив жанрів(обʼєднаний в рядок) або "Other", використовується при запиті інформації про фільми
+  matchMovieGenres = obj => {
+    const movieGenres = obj.reduce((acc, genreId) => {
+      const genre = this.genres.find(({ id }) => id === genreId);
+      if (genre) {
+        acc.push(genre.name);
+      }
+      return acc;
+    }, []);
+    //якщо жанр айді = пустий масив, то додає Інший жанр
+    if (movieGenres.length === 0) {
+      movieGenres.push('Other');
+    }
+    return movieGenres.join(', ');
+  };
+
+  //запит до серверу, що отримує дані популярних фільмів, повертає об'єкт готовий до рендеру
+  getPopularMovie = async () => {
+    // попереднє завантаження жанрів
+    await this.getGenres();
+    //власне, сам запит
+    const data = await axios(
+      `${BASE_URL}/trending/all/day?api_key=ae38d5c8baf36c9c4ca14e9456f3c0fd`
+    )
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error(`Error in request: ${response.status}`);
+        }
+
+        //обробка результату функцією getMoviesInfo прокидання отриманого обʼєкту в функцію обробник щоб витягнути необхідні поля
+        return this.getMoviesInfo(response.data.results);
+      })
+      .catch(e => console.error(e));
+
+    return data;
+  };
+
+  //запит на сервер, пошук фільмів за ключовими словами. при первинному запиті номер сторінки можна не прокидувати, в середині функції прописана логіка якщо номер сторінки undefined щоб скрипт не падав
+  getMovieByQuery = async (query, pageNumber) => {
+    //додаткова перевірка, чи підвантажились жанри
+    if (this.genres.length === 0) {
+      await this.getGenres();
+    }
+    // якщо запит оновлено, йде скидання до початкових значень
+    if (query !== this.query) {
+      this.query = query;
+      this.curentPage = 1;
+      this.totalPages = 0;
+      this.totalMovies = 0;
+    }
+    //якщо pageNumber = undefinеd(первинний запит що не задає номеру сторінки) запит переходить на сторінку 1, або поточну за замовчуванням
+    if (pageNumber) {
+      this.curentPage = pageNumber;
+    }
+
+    const data = await axios(
+      `${BASE_URL}/search/movie?api_key=ae38d5c8baf36c9c4ca14e9456f3c0fd&language=en-US&query=${query}&page=${this.curentPage}`
+    )
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error(`Error in request: ${response.status}`);
+        }
+
+        // перезапис значень, що використовуються для пагінації
+        this.totalMovies = response.data.total_results;
+        this.totalPages = response.data.total_pages;
+
+        //обробка результату, прокидання отриманого респонсу через функцію обробник
+        return this.getMoviesInfo(response.data.results);
+      })
+      .catch(e => console.error(e));
+
+    return data;
+  };
+
+  // запит, що підвантажує наступну порцію фільмів, перехід на вказану сторінку (пагінація). прокидується колбеком в методи бібліотеки пагінації
+  goToPage = pageNumber => {
+    return this.getMovieByQuery(this.query, pageNumber);
+  };
+
+  // функція обробник повернення масиву об"єктів даних про фільми. використовується при пошуку по назві завантаженні популярних фільмів, в інших файлах не використовується
+  getMoviesInfo = moviesArr => {
+    return moviesArr.map(movie => {
+      const movieInfo = {
+        title: movie.title, //назва
+        imgUrl: movie.poster_path
+          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+          : DEFAULT_IMG, // постер, або дефолтна картинка за відсутності постера
+        genres: this.matchMovieGenres(movie.genre_ids), // жанри
+        year: new Date(movie.release_date).getUTCFullYear(), //рік
+        id: movie.id, // ID не рендериться, використовується для отримання деталей по фільму
+      };
+      return movieInfo;
+    });
+  };
+
+  //запит, що отримує інформацію про фільм за ID, використовується для показу детальної інформації за фільмом
+  getMovieById = async id => {
+    const data = await axios(
+      `${BASE_URL}/movie/${id}?api_key=ae38d5c8baf36c9c4ca14e9456f3c0fd&language=en-US`
+    )
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error(`Error in request: ${response.status}`);
+        }
+
+        // обробка результату, прокидання отриманого респонса через функцію обробник
+        return this.getMoviesDetails(response.data);
+      })
+      .catch(e => console.error(e));
+
+    return data;
+  };
+
+  // функція обробник повертає об"єкт з детальною інформацією по фільму, в інших файлах не використовується
+  getMoviesDetails = movie => {
+    return {
+      title: movie.original_title, //назва
+      popularity: movie.popularity, //популярність
+      vote: movie.vote_average, // середній рейтинг
+      votes: movie.vote_count, // кількість голосів
+      imgUrl: movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : DEFAULT_IMG, // постер, або дефолтна картинка за відсутності постера
+      genres: this.matchMovieGenres(movie.genres), // жанри
+      about: movie.overview,
+      id: movie.id, // ID не рендериться, використовується для отримання деталей по фільму/трейлеру,
+    };
+  };
+
+  //запит, що отримує інформацію про трейлер фільму
+  getMoviesTrailer = async id => {
+    const data = await axios(
+      `${BASE_URL}/movie/${id}/videos?api_key=ae38d5c8baf36c9c4ca14e9456f3c0fd&language=en-US`
+    )
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error(`Error in request: ${response.status}`);
+        }
+        const result = response.data.results;
+        // обробка результату
+        // запит повертає обʼєкт що містить ключ до трейлеру[перший елемент масиву] та тізеру[другий елемент масиву]
+        if (result.length === 0) {
+          return null; // якщо запит повернув пустий масив функція повертає null
+        } else if (result.length > 0) {
+          const trailerObj = result.find(({ name }) =>
+            name.toLowerCase().includes('trailer')
+          );
+          if (trailerObj) {
+            return `https://www.youtube.com/watch?v=${trailerObj.key}`; //якщо є трейлер, то повертає рядок з адресою трейлера
+          } else {
+            return `https://www.youtube.com/watch?v=${result[0].key}`; //якщо тільки тизер, то відповідно адресу тизера
+          }
+        }
+
+        return response.data.results; //повертає масив об"єктів
+      })
+      .catch(e => console.error(e));
+
+    return data;
+  };
+}
+const api = new ApiClient(); //експортуємо екземпляр
+
+//приклад функції імітація запиту.  (у фінальному варіанті видалити)
+
+const getData = async () => {
+  const listOfPopularFilms = await api.getPopularMovie(); // популярні фільми
+  console.log('listOfPopularFilms', listOfPopularFilms);
+
+  const filmByQuery = await api.getMovieByQuery('cat'); // пошук за ім"ям
+  console.log('filmByQuery', filmByQuery);
+
+  const filmDetailsById = await api.getMovieById('1027159'); // пошук за іd
+  console.log('filmDetailsById', filmDetailsById);
+
+  const trailerInfo = await api.getMoviesTrailer('1027159'); // пошук трейлеру
+  console.log('trailerInfo', trailerInfo);
+};
+setTimeout(getData, 200);
+
+export default api;

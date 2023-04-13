4f38316bb27dda2fff94d1904c22e90966d18f36
Yurii Nechyporenko <2005usai@gmail.com>
2023-04-13 05:09:01 +0300

    working-space-created

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+
+
+export const refs = {
+    searchForm: document.querySelector("form"),
+    homeBtn: document.querySelector(".btn-home"),
+    myLibraryBtn: document.querySelector(".btn-library"),
+    logo: document.querySelector(".page-logo"),
+    moviesGallery: document.querySelector(".movies-list"),
+    buttonsList: document.querySelector(".tui-pagination"),
+  };

f2a6abe2add4f326e7d849a9b905b7bfc75d6613
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-04-15 14:11:04 +0300

    added local-storage to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/refs';
 import './js/pagination';
 import './js/render-functions';
 import './js/modal';
+import './js/local-storage';
 import './js/routing-pages';
 import './js/watched-markup';
 import './js/queue-markup';

f0cc0c3c11fe8e1487ad67709ed714520496171f
Aleksey-Dr <aprel181818@gmail.com>
2023-04-16 11:22:17 +0300

    Message if list of movies undefined when load
    Added message if list of movies undefined when load:
    Oops! Something went wrong.
    Changed the files:
    1) render-functions.js:
        - changed function "appendMovies".
    2) _film-cards.scss.
        - added styles.

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -24,7 +24,18 @@ export function appendMovies(movies) {
   
   refs.moviesGallery.innerHTML = '';
 
-  if (movies.length === 0) return;
+    if (movies.length === 0 || movies === undefined) {
+
+      refs.moviesGallery.insertAdjacentHTML('afterend',
+        `<div class="main-gallery-oops">
+          <div>
+            <img src="https://cdn.icon-icons.com/icons2/576/PNG/512/icon_imovie_icon-icons.com_54880.png">
+          </div>
+            <p><strong><span>Oops!</span> Something went wrong</strong></p>
+        </div>`);
+      return;
+
+    }
 
   const markup = movies
     .map(({ title, imgUrl, genres, year, id }) => {

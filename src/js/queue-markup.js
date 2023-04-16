741ffe0786728bf2fcb2a8e08b9964097e9bb68b
Stanislav Kintoriak <kintoriak@gmail.com>
2023-04-16 19:08:57 +0300

    improving-watched-markup.js-and-queue-markup.js-to-working-condition,-changed-placeholder-url
    1) improving-watched-markup.js-and-queue-markup.js-to-working-condition;
    2) changed-placeholder-url;
    3) commented some addition functionality of my markup functions for future review.

--- src/js/queue-markup.js
+++ src/js/queue-markup.js
@@ -1,6 +1,6 @@
 import refs from './refs.js';
 
-// import apiClient from './api-client.js';
+import api from './api-client.js';
 
 import { loadFilms } from './local-storage.js';
 
@@ -11,34 +11,46 @@ const QUEUE_KEY = 'queue';
 
 refs.queueBtn.addEventListener('click', markupQuoue);
 
-// тимчасово такий масив(поки немає нічого від local storage)
-// const queueMovies = [{ movie5 }, { movie6 }, { movie7 }, { movie8 }];
-// const queueFilms = localStorage.load(QUEUE_KEY);
 
-export function markupQuoue() {
-  if (
-    !refs.queueBtn.classList.contains('button--film-status-filter.is-active')
-  ) {
-    refs.queueBtn.classList.add('button--film-status-filter.is-active');
-    queueRef.disabled = true;
-    refs.watchedBtn.classList.remove('button--film-status-filter.is-active');
-    refs.watchedBtn.disabled = false;
-  }
+export async function markupQuoue() {
+  // if (
+  //   !refs.queueBtn.classList.contains('button--film-status-filter.is-active')
+  // ) {
+  //   refs.queueBtn.classList.add('button--film-status-filter.is-active');
+  //   queueRef.disabled = true;
+  //   refs.watchedBtn.classList.remove('button--film-status-filter.is-active');
+  //   refs.watchedBtn.disabled = false;
+  // }
 
-  console.log(2);
-  // перевірка роботи кнопки
+  const queueMoviesIds = loadFilms(QUEUE_KEY);
+
+  let queueMovies = [];
+
+  queueMoviesIds.map(async ({ id }) => {
+    if (id) {
+      // console.log(id);
+
+      const chosenMovieByID = await api.getMovieById(id);
+
+      queueMovies.push(chosenMovieByID);
+    }
+  });
+
+  console.log(queueMovies);
+
+
 
   // should add styles for .watched-queue-button--active!!!
 
   //   placeholder (заглушка)
-  if (!queueMovies.length) {
-    refs.moviesGallery.innerHTML = `
-              <li class="empty">
-              <img class="empty-library-image" src="https://gifdb.com/gif/popcorn-brown-claymation-0sz0dt7bhumu7ifv.html?embed=true" alt="Empty gallery.Add something)" />
-              <p class="empty-library-notification">No movies here. Please add something to queue.</p>
-              </li>`;
-    // should add styles for .empty!!!
-    return;
-  }
+  // if (!queueMovies.length) {
+  //   refs.moviesGallery.innerHTML = `
+  //             <li class="empty">
+  //             <img class="empty-library-image" src="https://cdn.icon-icons.com/icons2/576/PNG/512/icon_imovie_icon-icons.com_54880.png" alt="Empty gallery.Add something)" />
+  //             <p class="empty-library-notification">No movies here. Please add something to queue.</p>
+  //             </li>`;
+  //   // should add styles for .empty!!!
+  //   return;
+  // }
   appendMovies(queueMovies);
 }

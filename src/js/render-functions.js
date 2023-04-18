e3ace802f935bbf882ccda0fe88259deacade8ca
usai2005 <88884179+usai2005@users.noreply.github.com>
2023-04-19 00:46:08 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -25,7 +25,7 @@ export function appendMovies(movies) {
       refs.galleryOps.innerHTML = '';
       
       refs.galleryOps.insertAdjacentHTML(
-        'beforeend', `<div class="main-gallery-oops"><p><strong><span>Oops!</span> Something went wrong</strong></p><div><img src="https://cdn.icon-icons.com/icons2/576/PNG/512/icon_imovie_icon-icons.com_54880.png"></div></div>`
+        'beforeend', `<div class="main-gallery-oops"><p><strong><span>Oops!</span> THE LIBRARY IS EMPTY. ADD SOMETHING</strong></p><div><img src="https://cdn.icon-icons.com/icons2/576/PNG/512/icon_imovie_icon-icons.com_54880.png"></div></div>`
     );
     return;
   }
